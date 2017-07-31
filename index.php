e135f0fe189ea853414460dc3f914966ebf1a326
Marty Bonacci <martybonacci@gmail.com>
2017-07-31 09:50:14 -0600

    playing with bootstrap

--- index.php
+++ index.php
@@ -37,95 +37,23 @@
 		<!-- custom CSS stylesheet -->
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 
-		<title>CSS Mockup Activity</title>
+		<title>Bootstrap Activity</title>
 	</head>
-	<body class="sfooter">
-		<div class="sfooter-content">
-
-			<header>
-				<div class="headerContainer">
-					<h1>Dylan McDonald</h1>
-					<em>Computer Scientist. Cat Lover. Picard Fan.</em>
-				</div>
-			</header>
-
-			<main>
-
-				<!-- INSTRUCTIONS PANEL -->
-				<section>
-					<div class="instructionscontrainer">
-						<div class="instructions">
-							<div class="pull-left">
-								<h2>Instructions</h2>
-								<ol class="instructiontext">
-									<li>
-										Fork and clone the following GitHub repository, and create a new project and deployment.<br>
-										<a href="https://github.com/rlewis2892/css-mockup-activity" target="_blank">https://github.com/rlewis2892/css-mockup-activity</a>
-									</li>
-									<li>Edit the stylesheet, adding CSS to make the page look like the mockup. <a href="images/mockup.jpg" target="_blank">Mockup Image</a></li>
-									<li>Have fun! The solution will be demonstrated in class. <small>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</small></li>
-								</ol>
-							</div>
-							<div class="pull-right">
-								<img class="captainimage" src="images/captain-janeway.png" alt="captain janeway" />
-							</div>
-							<div class="clearfix"></div>
-						</div><!--/.instructions-->
-					</div><!--/.container-->
-				</section>
-
-				<!-- SECTION ONE -->
-				<section>
-					<div class="stunningcontainer">
-						<div>
-						<img class="pull-left stunningimage" src="images/stunning.jpg" alt="captain picard" />
-						</div>
-						<div class="pull-right stunning-text">
-						<h2>A Day in the Life...</h2>
-						<p class="stunning-paragraph">Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
-						<p class="stunning-paragraph">Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span class="grom-text">Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
-						</div>
-						<div class="clearfix"></div>
-					</div>
-				</section>
-
-				<!-- SECTION TWO -->
-				<section>
-					<div class="container">
-						<h2>Summary of Profe$$ional Skillz</h2>
-						<table>
-							<tr>
-								<th>Languages</th>
-								<th>Software</th>
-								<th>Other</th>
-							</tr>
-							<tr>
-								<td>Java</td>
-								<td>Linux Server</td>
-								<td>Certified Ethical Hacker</td>
-							</tr>
-							<tr>
-								<td>PHP</td>
-								<td>Metasploit</td>
-								<td>Star Trek Trivia Champion</td>
-							</tr>
-							<tr>
-								<td>Klingon</td>
-								<td>Microsoft Office</td>
-								<td>Meme Master</td>
-							</tr>
-						</table>
-					</div><!--/.container-->
-				</section>
-
-			</main>
-		</div><!--/.sfooter-content-->
-
-		<footer>
-			<div class="container">
-				<h4>&copy; 2017 A USS Berners-Lee Production</h4>
+	<body>
+		<!-- begin grid layout -->
+		<div class="container">
+			<div class="row">
+			<div class="col-md-6">
+				<h2>Column 1</h2>
+				<p>Using just the .col-md-* grid classes, you can create a basic responsive layout where columns start out full-width and stacked vertically on mobile devices and tablets (the XS-SM breakpoints), then becoming horizontal on desktop screens (MD - LG breakpoints, 992px wide and up). See the following example: Bootstrap Basic Grid Example.</p>
+			<div class="col-md-6">
+				<h2>Column 2</h2>
+				<p>In the above prefixes, you can also see a two-letter reference to a Bootstrap breakpoint: xs, sm, md, and lg. This way, you can create elements that have varying column widths for different Bootstrap breakpoints. Grid classes apply to devices with screen widths greater than or equal to the breakpoint sizes.</p>
 			</div>
-		</footer>
+
+			</div>
+			</div>
+		</div>
 	</body>
 </html>
 

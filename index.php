b35cbd68bd85d01a6ccc64fe626ac54b153cc0fc
Marty Bonacci <martybonacci@gmail.com>
2017-07-31 10:04:17 -0600

    playing with bootstrap a little more.........

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8" />
-		<meta http-equiv"X-UA-Compatible" content="IE=edge"> <!-- needed for Internet Explorer's rendering engine  -->
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta charset="utf-8"/>
+		<meta http-equiv
+		"X-UA-Compatible" content="IE=edge"> <!-- needed for Internet Explorer's rendering engine  -->
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!-- Font Awesome -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet">
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet">
 
 		<!-- Custom CSS Goes Here -->
 
@@ -27,15 +31,14 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 
-
-
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 
 		<!-- custom CSS stylesheet -->
-		<link rel="stylesheet" href="css/style.css" type="text/css" />
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 
 		<title>Bootstrap Activity</title>
 	</head>
@@ -43,19 +46,28 @@
 		<!-- begin grid layout -->
 		<div class="container">
 			<div class="row">
-			<div class="col-md-6">
-				<h2>Column 1</h2>
-				<p>Using just the .col-md-* grid classes, you can create a basic responsive layout where columns start out full-width and stacked vertically on mobile devices and tablets (the XS-SM breakpoints), then becoming horizontal on desktop screens (MD - LG breakpoints, 992px wide and up). See the following example: Bootstrap Basic Grid Example.</p>
-			</div>
-			<div class="col-md-6">
-				<h2>Column 2</h2>
-				<p>In the above prefixes, you can also see a two-letter reference to a Bootstrap breakpoint: xs, sm, md, and lg. This way, you can create elements that have varying column widths for different Bootstrap breakpoints. Grid classes apply to devices with screen widths greater than or equal to the breakpoint sizes.</p>
-			</div>
+				<div class="col-md-6">
+					<h2>Column 1</h2>
+					<p>Using just the .col-md-* grid classes, you can create a basic responsive layout where columns start
+						out full-width and stacked vertically on mobile devices and tablets (the XS-SM breakpoints), then
+						becoming horizontal on desktop screens (MD - LG breakpoints, 992px wide and up). See the following
+						example: Bootstrap Basic Grid Example.</p>
+				</div>
+				<div class="col-md-6">
+					<h2>Column 2</h2>
+					<p>In the above prefixes, you can also see a two-letter reference to a Bootstrap breakpoint: xs, sm, md,
+						and lg. This way, you can create elements that have varying column widths for different Bootstrap
+						breakpoints. Grid classes apply to devices with screen widths greater than or equal to the breakpoint
+						sizes.</p>
+				</div>
 			</div>
 			<div class="row">
-				<div class="col-xs-12">
+				<div class="col-xs-12 col-md-6">
 					<h2>Column 3</h2>
-					<p>Here is some simple boilerplate code to help you get started in creating a main navigation bar using Bootstrap. This navbar automatically collapses and hides the links under a mobile toggle menu on small screens. For a more detailed example, with additional functionality, refer to the documentation here: </p>
+					<p>Here is some simple boilerplate code to help you get started in creating a main navigation bar using
+						Bootstrap. This navbar automatically collapses and hides the links under a mobile toggle menu on small
+						screens. For a more detailed example, with additional functionality, refer to the documentation
+						here: </p>
 				</div>
 			</div>
 
